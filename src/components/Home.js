040680f14f8cb1a88a05e66ee45e50ed3cb1d074
Koby Fink <finkkoby@icloud.com>
2024-02-19 18:16:04 -0500

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id='home'>
+    <h1 style={divStyle}>{ name } is a Web Developer from { city }</h1>
+  </div>
+)
+}
+
+const divStyle = {
+  color: 'firebrick'
 }
 
 export default Home;
